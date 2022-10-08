bddb67c379cec5940db02d62ee1434ec510c0cb0
Diego Victor <diegovictorgonzaga@gmail.com>
2022-10-08 00:06:12 -0300

    Create a safe JSON parse

--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,12 @@ const [, , project = 'tsconfig.json'] = process.argv;
 const ROOT_PATH = process.cwd();
 const tsconfigPath = path.resolve(ROOT_PATH, project);
 
+function safeParse(raw: string) {
+  try {
+    return JSON.parse(raw);
+  } catch ({ message }) {
+    console.log(`Error in tsconfig.json: ${message}`);
+  }
+  return null;
+}
+
