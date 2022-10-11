80a728968328c0b023e101cfaeb1612f822288a2
Diego Victor <diegovictorgonzaga@gmail.com>
2022-10-11 09:47:06 -0300

    Improve variable name

--- src/tsc-paths-replacer.ts
+++ src/tsc-paths-replacer.ts
@@ -88,9 +88,9 @@ function getModulePath(moduleName: string, file: string) {
 
 const IMPORT_REGEX = /(?:require\(|import\(?|from) ?['"]([^'"]*)['"]\)?/g;
 async function replace(text: string, file: string) {
-  const matches = Array.from(text.matchAll(IMPORT_REGEX));
+  const imports = Array.from(text.matchAll(IMPORT_REGEX));
 
-  for (const [raw, match] of matches) {
+  for (const [raw, match] of imports) {
     const modulePath = getModulePath(match, file);
     if (modulePath) {
       text = text.replace(raw, raw.replace(match, modulePath));
